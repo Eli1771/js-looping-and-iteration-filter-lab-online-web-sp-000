227022c64ae6570697fddc7e6436ad1aeefeb5a9
elijahmm17 <elijahmm17@gmail.com>
2019-09-27 23:03:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Code your solution in this file
 
+console.log(drivers);
+
 function findMatching(drivers, name) {
   return drivers.filter(function(driver) {driver.toUpperCase() === name.toUpperCase()});
 }
