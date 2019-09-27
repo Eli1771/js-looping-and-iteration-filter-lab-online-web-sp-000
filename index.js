8b52dba5229db76ce77b5a7ea2a5e726678c413c
elijahmm17 <elijahmm17@gmail.com>
2019-09-27 23:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function findMatching(drivers, name) {
 }
 
 function fuzzyMatch(drivers, string) {
-  return drivers.filter(function(driver) {return driver.toUpperCase() == name.toUpperCase()});
+  return drivers.filter(function(driver) {return driver == name});
 }
 
 function matcName() {
