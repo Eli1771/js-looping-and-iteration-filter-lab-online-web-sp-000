a9683a101663a2574b0f27a6337ff6179b7b8917
elijahmm17 <elijahmm17@gmail.com>
2019-09-27 23:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(drivers, name) {
-  drivers.filter(function(driver) {driver.toUpperCase() === name.toUpperCase()});
+  return drivers.filter(function(driver) {driver.toUpperCase() === name.toUpperCase()});
 }
 
 function fuzzyMatch() {
